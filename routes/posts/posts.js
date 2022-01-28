d66f19e1035f5339ac31347a0b32e7d7b22fb698
FATMA AKIN <97286722+fatmakin@users.noreply.github.com>
2022-01-29 00:00:35 +0300

    Update posts.js

--- routes/posts/posts.js
+++ routes/posts/posts.js
@@ -4,10 +4,11 @@ let router = express.Router();
 
 let postsController = require('../../controllers/Posts')
 
-router.post('/',postsController.add);
-router.get('/',postsController.fetchAll);
-router.get('/:id',postsController.fetchisDiscount);
-//router.put('/:id',postsController.update);
-//router.delete('/:id',postsController.delete);
+router.post('/',postsController.add);// insert data
+router.get('/',postsController.fetchAll);// all data list
+router.get('/:id',postsController.fetchSingle);// data list by productid
+router.get('/:id',postsController.fetchisDiscount);//  data list by isdiscount
+router.put('/:id',postsController.update);// update data by productid
+router.delete('/:id',postsController.delete);// delete data by productid
 
-module.exports = router;
+module.exports = router; //  The module requires Express and then uses it to create a Router object.
