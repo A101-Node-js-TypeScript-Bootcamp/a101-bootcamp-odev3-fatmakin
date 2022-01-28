0cbcd519f1aa3e7635562db67fcfaf063c84ea38
FATMA AKIN <97286722+fatmakin@users.noreply.github.com>
2022-01-28 22:15:20 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express()
-const endPoint = require('./routes/api')
-
-app.use('/api',endPoint)
-app.listen(3000, () => {
-    console.log('Server is running!!')
-})
