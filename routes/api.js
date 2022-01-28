180c54e97ff8d11681ef225db3481ce779ce3923
FATMA AKIN <97286722+fatmakin@users.noreply.github.com>
2022-01-28 22:15:06 +0300

    Delete routes directory

--- routes/api.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-let router = express.Router();
-const postsEndpoint = require('./posts/posts')
-
-router.use('/posts',postsEndpoint)  //localhost:3000/api/posts
-module.exports = router;
